bb57fa9a5355703a65cab2b28bd22fc3096d9edf
iamadlet <adlet.zhumagaliev2003@gmail.com>
2026-04-09 12:04:45 +0200

    feat: created AppTextStyle enum & Color sets

---
+++ WeatherApp/Utilities/AppTextStyle.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+enum AppTextStyle {
+    case header
+    case title
+    case temperatureTitle
+    case subtitle
+    
+    case weatherDescription
+    case temperatureForecast
+}
+
+
+extension AppTextStyle {
+    var font: UIFont {
+        switch self {
+        case .header:
+            return .systemFont(ofSize: 32, weight: .bold)
+        case .title:
+            return .systemFont(ofSize: 32, weight: .medium)
+        case .temperatureTitle:
+            return .systemFont(ofSize: 92, weight: .thin)
+        case .subtitle:
+            return .systemFont(ofSize: 18, weight: .medium)
+        case .weatherDescription:
+            return .systemFont(ofSize: 14, weight: .regular)
+        case .temperatureForecast:
+            return .systemFont(ofSize: 15, weight: .medium)
+        }
+        
+    }
+}
